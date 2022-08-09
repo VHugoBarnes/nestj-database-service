e36704eab592fa710ab01b4278ec77a057fa5b05
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2022-08-08 21:49:27 -0500

    Implement user controller functions

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+
+import { UserService } from "src/services/user/user.service";
 
 @Controller("user")
-export class UserController { }
+export class UserController {
+  constructor(private userService: UserService) {
+
+  }
+
+  @Get("/")
+  async getAllUsers() {
+    const users = await this.userService.findAll();
+
+    return {
+      ok: true,
+      response: {
+        data: {
+          users: users
+        },
+        message: "[sucess]",
+        redirect: false
+      }
+    };
+  }
+
+  @Get("/:id")
+  async getUserById(@Param("id") id: string) {
+    const user = await this.userService.findById(id);
+
+    return {
+      ok: true,
+      response: {
+        data: {
+          user: user
+        },
+        message: "[sucess]",
+        redirect: false
+      }
+    };
+  }
+
+  @Post("/")
+  async createUser(@Body() body: any) {
+    const user = await this.userService.create(body.payload);
+
+    return {
+      ok: true,
+      response: {
+        data: {
+          user: user,
+        },
+        message: "[user-created]",
+        redirect: true
+      }
+    };
+  }
+}
